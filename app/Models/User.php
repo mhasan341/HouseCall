0a4811eb2050bca6b941a7eb25bfb0644130128d
Mahmudul Hasan <mhasan341@gmail.com>
2024-07-12 21:45:27 +0600

    Initial Commit Initialize Laravel project and setting up base

---
+++ app/Models/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use DateTimeInterface;
+use Hash;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, Notifiable, HasFactory;
+
+    public $table = 'users';
+
+    protected $hidden = [
+        'remember_token',
+        'password',
+    ];
+
+    protected $dates = [
+        'email_verified_at',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'remember_token',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
